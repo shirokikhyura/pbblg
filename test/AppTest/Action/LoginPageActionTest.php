93881a6b78a33f9560b57c2f00b110a85c33545c
yurchik <shirokikh.yura@gmail.com>
2018-04-12 08:20:54 +0300

    Coverage test

---
+++ test/AppTest/Action/LoginPageActionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppTest\Action;
+
+use App\Action\LoginAction;
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Expressive\Router\RouterInterface;
+use Zend\Expressive\Template\TemplateRendererInterface;
+
+class LoginPageActionTest extends TestCase
+{
+    /** @var RouterInterface */
+    protected $router;
+
+    protected function setUp()
+    {
+        $this->router = $this->prophesize(RouterInterface::class);
+    }
+
+    public function testReturnsHtmlResponseWhenTemplateRendererProvided()
+    {
+        $renderer = $this->prophesize(TemplateRendererInterface::class);
+        $renderer
+            ->render('app::login')
+            ->willReturn('');
+
+        $homePage = new LoginAction($this->router->reveal(), $renderer->reveal());
+
+        $response = $homePage->process(
+            $this->prophesize(ServerRequestInterface::class)->reveal(),
+            $this->prophesize(DelegateInterface::class)->reveal()
+        );
+
+        $this->assertInstanceOf(HtmlResponse::class, $response);
+    }
+}
